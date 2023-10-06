8fd5eb1c56212b3ef94be542a4ee32e123bbe8e7
Akshat Girdhar <akshatgirdhar5@gmail.com>
2023-10-06 14:22:39 +0530

    Update settings.py 3

--- FootballProject/settings.py
+++ FootballProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)61p2=u(ijxt^p+iomxa-l(*fo93eo*qos$n^hwagi1+-1cqq#
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost','footballclubzdjango.onrender.com']
 
 
